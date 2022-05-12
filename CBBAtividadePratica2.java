7072fe4e365062db5fd51df14e1fe9a4169a352b
Guilherme Costa <gui-costaquino@hotmail.com>
2022-05-11 21:03:44 -0300

    Adicionando resolução das questões práticas 2, 3 e 4 do livro Chagas, Baruque & Baruque(2010)

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica2{
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = 5;
+        int soma = num1 + num2 + num3;
+        float media = soma / 3f;
+        System.out.println("A soma das variáveis é: " + soma);
+        System.out.println("A média das variáveis é: " + media);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+## Resolução
+
+1) A resolução se encontra no arquivo CBBAtividadePratica2.java .
+2) A resolução se encontra no arquivo CBBAtividadePratica3.java .
+3) A resolução se encontra no arquivo CBBAtividadePratica4.java .
 
